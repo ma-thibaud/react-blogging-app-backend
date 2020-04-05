23cabaea8c60d940e1e0cf4ddad61899ce629c01
Mathias Thibaud <math.thibaud@gmail.com>
2020-04-05 11:25:30 -0700

    First commit

---
+++ src/build/precache-manifest.42814412e58e13a59f1710a0fd577650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7461e2318536248a6367fd655161a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e340ede651e0902ae81",
+    "url": "/static/css/main.84b4ca96.chunk.css"
+  },
+  {
+    "revision": "01a96dd44287e25302f0",
+    "url": "/static/js/2.c7153f9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c7153f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e340ede651e0902ae81",
+    "url": "/static/js/main.00a9b3a4.chunk.js"
+  },
+  {
+    "revision": "c462ffc6248aec5ff241",
+    "url": "/static/js/runtime-main.c30ffdcb.js"
+  }
+]);
